b2866bb976f47390a6d8cc9d46fda2324a4139e2
Jason Moss <jmoss35@ut.utm.edu>
2023-09-13 11:48:23 -0500

    Created intial files

---
+++ group_hw_01/Steganography.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file Steganography.cpp
+ * @author Jason Moss
+ * @date 2023-09-13
+ * @brief Method definitions for the Steganography class
+ * 
+ * Defines the methods for the Steganography class
+ */
+
+#include "Steganography.h"
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int getNthBit(char cipherChar, int n){}
+
+void readImage(string fileName){}
+
+void printImage(string fileName){}
+
+void readCipherText(string fileName){}
+
+void printCipherText(string fileName){}
+
+void cleanImage(){}
+
+void encipher(){}
+
+void decipher(){}
---
+++ group_hw_01/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @author Jason Moss
+ * @date 2023-09-13
+ * @brief Implements created class or classes in roder to carry out steganography
+ * 
+ * COME BACK TO THIS DESCRIPTION!!!!!!!!!!!!!!!
+ */
+
+
+
+#include "Steganography.h"
+#include <iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main() {
+  
+  
+  return 0;
+}
